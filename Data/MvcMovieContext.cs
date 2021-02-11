62123122614e4cde2ca9f4ee70ca869b3823e499
Xavier <xavier.chemin@viacesi.fr>
2021-02-11 10:34:31 +0100

    start

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TParchitecture.Models;
+
+namespace MvcMovie.Data
+
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+
+    }
+}
